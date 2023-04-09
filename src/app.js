a3591b020330e903b4fefb851c22022f852fed7d
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-04-09 14:30:58 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -5,28 +5,49 @@ import "./style.css";
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let typeExtension = [".com", ".net", ".cr", ".biz", ".us", ".io"];
+let typeExtension = [
+  ".com",
+  ".org",
+  ".net",
+  "int",
+  ".edu",
+  ".gov",
+  ".cr",
+  ".us",
+  ".biz",
+  "info",
+  ".name",
+  ".pro",
+  ".io"
+];
 
-let temp = "";
+let combDomain = [];
+for (let varPron in pronoun) {
+  for (let varAdj in adj) {
+    for (let varNou in noun) {
+      combDomain.push(pronoun[varPron] + adj[varAdj] + noun[varNou]);
+    }
+  }
+}
 
-let domain = typeExtension.map((item, index) => {
-  temp = "<li>" + adj[index] + item + "</li>";
-  return temp;
+let domainCompleto = [];
+
+for (let ext in typeExtension) {
+  for (let title in combDomain) {
+    domainCompleto.push(
+      "<li>" + combDomain[title] + typeExtension[ext] + "</li>"
+    );
+  }
+}
+let domainConsola = "";
+domainCompleto.map((item, index) => {
+  domainConsola = item.replace("<li>", "");
+  domainConsola = domainConsola.replace("</li>", "");
+  console.log(domainConsola);
 });
-let tempDomain = "";
 
 window.onload = function() {
   //write your code here
-  console.log(domain);
-  document.querySelector("#excuse").innerHTML = domain;
-
-  for (let varPron in pronoun) {
-    console.log(pronoun[varPron]);
-    for (let varAdj in adj) {
-      for (let varNou in noun) {
-        tempDomain = pronoun[varPron] + adj[varAdj] + noun[varNou];
-        console.log(tempDomain);
-      }
-    }
-  }
+  //console.log(domain);
+  document.querySelector("#list").innerHTML = domainCompleto;
 };
