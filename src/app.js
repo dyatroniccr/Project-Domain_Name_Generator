c316b663faa4656061add3180a4f8d4140be3a24
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-04-09 11:36:46 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@
 import "bootstrap";
 import "./style.css";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [];
 
 function randomNum(arrSelect) {
   let number = Math.floor(Math.random() * arrSelect.length);
