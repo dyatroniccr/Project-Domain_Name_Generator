9ef74e93e3aaccaf3eb255fade09147d6384224d
Jonathan A. Montero C <dyatroniccr@gmail.com>
2023-04-09 12:03:27 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -5,30 +5,25 @@ import "./style.css";
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let domain = [];
+let typeExtension = [];
 
 function randomNum(arrSelect) {
   let number = Math.floor(Math.random() * arrSelect.length);
   return arrSelect[number];
 }
 
+let domain = pronoun.map((item, index) => {
+  let temp = item + adj[index];
+  return temp;
+});
 let excuseView = () => {
-  return (
-    "<h4 >" +
-    randomNum(who) +
-    " " +
-    randomNum(action) +
-    " " +
-    randomNum(what) +
-    " " +
-    randomNum(when) +
-    "</h4>"
-  );
+  return "<h5 >" + randomNum(pronoun) + " " + randomNum(adj) + " " + "</h5>";
 };
 
 window.onload = function() {
   //write your code here
+  console.log(domain);
   document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#excuse").innerHTML = excuseView();
+    document.querySelector("#excuse").innerHTML = domain;
   });
 };
